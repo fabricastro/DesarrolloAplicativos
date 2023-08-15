2e870312041216b5fdbe83d935bec0093daa19d1
fabricastro <fabricastrook@gmail.com>
2023-08-15 17:09:21 -0300

    first commit

---
+++ src/components/Bienvenida.jsx
@@ -0,0 +1,15 @@
+export const Bienvenida = () => {
+    return(
+        <section className="flex flex-col justify-center items-center gap-4 ">
+
+        <div className="p-8 bg-red-400">
+            <h1 className="text-3xl p-10">Fabricio Castro</h1>                       
+        </div>
+        <div className="p-8 bg-red-400">
+            <h1 className="text-3xl p-10">Matias Castro</h1>                       
+        </div>
+
+        </section>
+
+    )
+}
